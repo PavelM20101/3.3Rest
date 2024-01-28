96e834915c1246bade52716123c3ffcc6255aa17
Hozyain <qwpo01_0101@mail.ru>
2024-01-28 15:06:49 +0400

    На доработке

--- src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/config/ApplicationSecurityConfig.java
+++ src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/config/ApplicationSecurityConfig.java
@@ -1,19 +1,12 @@
 package ru.itsinfo.springbootsecurityusersbootstrap.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import ru.itsinfo.springbootsecurityusersbootstrap.config.handler.SuccessUserHandler;
 import ru.itsinfo.springbootsecurityusersbootstrap.service.AppService;
 
@@ -23,39 +16,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SuccessUserHandler successUserHandler;
 
-    // сервис, с помощью которого тащим пользователя
     private final AppService appService;
 
     private final PasswordEncoder passwordEncoder;
 
-    // класс, в котором описана логика перенаправления пользователей по ролям
-
-//    private final SimpleUrlAuthenticationSuccessHandler authenticationSuccessHandler;
-//
-//    // класс, в котором описана логика при неудачной авторизации
-//    private final SimpleUrlAuthenticationFailureHandler authenticationFailureHandler;
-//
-//    // класс, в котором описана логика при удачной авторизации
-//    private final SimpleUrlLogoutSuccessHandler urlLogoutSuccessHandler;
-//
-//    // класс, в котором описана логика при отказе в доступе
-//    private final AccessDeniedHandler accessDeniedHandler;
-
     @Autowired
     public ApplicationSecurityConfig(SuccessUserHandler successUserHandler,
                                      AppService appServiceTmp,
-                                     PasswordEncoder passwordEncoder/*,
-                                     SimpleUrlAuthenticationSuccessHandler authenticationSuccessHandler,
-                                     SimpleUrlAuthenticationFailureHandler authenticationFailureHandler,
-                                     SimpleUrlLogoutSuccessHandler urlLogoutSuccessHandler,
-                                     AccessDeniedHandler accessDeniedHandler*/) {
+                                     PasswordEncoder passwordEncoder) {
         this.successUserHandler = successUserHandler;
         this.appService = appServiceTmp;
         this.passwordEncoder = passwordEncoder;
-//        this.authenticationSuccessHandler = authenticationSuccessHandler;
-//        this.authenticationFailureHandler = authenticationFailureHandler;
-//        this.urlLogoutSuccessHandler = urlLogoutSuccessHandler;
-//        this.accessDeniedHandler = accessDeniedHandler;
     }
 
     @Override
