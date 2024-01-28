e0b162f1eb6bbd24d9a6134d9818390a1203fc62
Hozyain <qwpo01_0101@mail.ru>
2024-01-28 12:46:09 +0400

    На доработке

--- src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/SpringBootSecurityUsersBootstrapApplication.java
+++ src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/SpringBootSecurityUsersBootstrapApplication.java
@@ -13,43 +13,43 @@ import ru.itsinfo.springbootsecurityusersbootstrap.repository.UserRepository;
 import java.util.HashSet;
 
 @SpringBootApplication
-public class SpringBootSecurityUsersBootstrapApplication implements CommandLineRunner {
+public class SpringBootSecurityUsersBootstrapApplication {
 
-    private final RoleRepository roleRepository;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+//    private final RoleRepository roleRepository;
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public SpringBootSecurityUsersBootstrapApplication(RoleRepository roleRepository,
-                                                       UserRepository userRepository,
-                                                       PasswordEncoder passwordEncoder) {
-        this.roleRepository = roleRepository;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+//    @Autowired
+//    public SpringBootSecurityUsersBootstrapApplication(RoleRepository roleRepository,
+//                                                       UserRepository userRepository,
+//                                                       PasswordEncoder passwordEncoder) {
+//        this.roleRepository = roleRepository;
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityUsersBootstrapApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Role admin = new Role("ROLE_ADMIN");
-        Role user = new Role("ROLE_USER");
-        roleRepository.save(admin);
-        roleRepository.save(user);
-        roleRepository.save(new Role("ROLE_GUEST"));
-
-        userRepository.save(new User("Василий", "Уткин", 49, "admin@mail.com",
-                passwordEncoder.encode("admin"),
-                new HashSet<>() {{
-                    add(admin);
-                    add(user);
-                }}));
-        userRepository.save(new User("Дмитрий", "Губерниев", 46, "user@mail.com",
-                passwordEncoder.encode("user"),
-                new HashSet<>() {{
-                    add(user);
-                }}));
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Role admin = new Role("ROLE_ADMIN");
+//        Role user = new Role("ROLE_USER");
+//        roleRepository.save(admin);
+//        roleRepository.save(user);
+//        roleRepository.save(new Role("ROLE_GUEST"));
+//
+//        userRepository.save(new User("Василий", "Уткин", 49, "admin@mail.com",
+//                passwordEncoder.encode("admin"),
+//                new HashSet<>() {{
+//                    add(admin);
+//                    add(user);
+//                }}));
+//        userRepository.save(new User("Дмитрий", "Губерниев", 46, "user@mail.com",
+//                passwordEncoder.encode("user"),
+//                new HashSet<>() {{
+//                    add(user);
+//                }}));
+//    }
 }
