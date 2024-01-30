a6c8a2f8b14a7d42a7504c466481a781d24c8562
Hozyain <qwpo01_0101@mail.ru>
2024-01-30 11:45:13 +0400

    !

--- src/main/resources/static/js/its.js
+++ src/main/resources/static/js/its.js
@@ -2,16 +2,14 @@ $(document).ready(function () {
     // Обработчик клика по кнопкам редактирования
     $('.edit-button').on('click', function (event) {
         event.preventDefault(); // Предотвращение стандартного действия браузера
-
-        // Отображение модального окна редактирования пользователя и загрузка его содержимого
+        //Отображение модального окна редактирования пользователя и загрузка его содержимого
         $('#user-profile').modal("show").find('.modal-dialog').load($(this).attr('href'), function(response, status, xhr) {
             if (xhr.status === 404) {
-                // Если страница не найдена, перенаправляем на страницу администратора
+                // Если страница не найдена то перенаправляем на страницу администратора
                 $(location).attr('href', '/admin');
             }
             // Обновление заголовка модального окна
             $('#user-profile .modal-header h3').text('Edit User');
-
             // Настройка кнопки сохранения
             let submit = $('#user-profile .modal-footer .submit');
             submit.text('Save'); // Изменение текста кнопки
@@ -19,11 +17,9 @@ $(document).ready(function () {
             $('#user-profile #method').val("patch"); // Установка метода для формы редактирования
         });
     });
-
     // Обработчик клика по кнопкам удаления
     $('.delete-button').on('click', function (event) {
         event.preventDefault(); // Предотвращение стандартного действия браузера
-
         // Отображение модального окна удаления пользователя и загрузка его содержимого
         $('#user-profile').modal("show").find('.modal-dialog').load($(this).attr('href'), function(response, status, xhr) {
             if (xhr.status === 404) {
@@ -32,7 +28,6 @@ $(document).ready(function () {
             }
             // Обновление заголовка модального окна
             $('#user-profile .modal-header h3').text('Delete User');
-
             // Удаление поля ввода пароля и установка других полей только для чтения
             $('#user-profile #password-div').remove();
             $("#user-profile #firstName").prop("readonly", true);
@@ -40,7 +35,6 @@ $(document).ready(function () {
             $("#user-profile #age").prop("readonly", true);
             $("#user-profile #email").prop("readonly", true);
             $("#user-profile #roles").prop("disabled", true);
-
             // Настройка кнопки удаления
             let submit = $('#user-profile .modal-footer .submit');
             submit.text('Delete'); // Изменение текста кнопки
